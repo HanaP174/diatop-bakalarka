51886ead2265f22b08da4cd26127114bf6f9ef64
Hana <pilkova.hana@gmail.com>
2023-02-20 10:42:08 +0100

    resource server

---
+++ resource/src/main/java/com/example/resource/ResourceApplication.java
@@ -0,0 +1,75 @@
+package com.example.resource;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+@SpringBootApplication
+@RestController
+public class ResourceApplication {
+
+  @RequestMapping("/")
+  @CrossOrigin(origins = "*", maxAge = 3600,
+          allowedHeaders={"x-auth-token", "x-requested-with", "x-xsrf-token"})
+  public Message home() {
+    return new Message("Hello World");
+  }
+
+  @Configuration
+  @EnableWebSecurity
+  protected static class SecurityConfiguration {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+      http
+              .cors()
+              .and()
+              .authorizeHttpRequests()
+              .anyRequest()
+              .authenticated();
+      return http.build();
+    }
+  }
+
+  public static void main(String[] args) {
+    SpringApplication.run(ResourceApplication.class, args);
+  }
+
+  class Message {
+    private String id = UUID.randomUUID().toString();
+    private String content;
+
+    public Message(String content) {
+      this.content = content;
+    }
+
+    public Message(String id, String content) {
+      this.id = id;
+      this.content = content;
+    }
+
+    public String getId() {
+      return id;
+    }
+
+    public void setId(String id) {
+      this.id = id;
+    }
+
+    public String getContent() {
+      return content;
+    }
+
+    public void setContent(String content) {
+      this.content = content;
+    }
+  }
+}
